a90bb532fe4c136b82f62526920f47c101d73613
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-06-30 10:28:35 +0530

    complete todolist component

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import './ToDoList.css'
 
-function ToDoList() {
+function ToDoList({todos, onToggle}) {
   return (
-    <div>ToDoList</div>
+    <div className='container'>
+        <ul>
+            {todos.map((todo, index)=>(
+                <li key={todo.id}>
+                    <span className='content'>{todo.text}</span>
+                    <span className={todo.completed ? 'completed':'pending'}>{todo.completed ? 'Completed': 'Pending'}</span>
+                    <button className='btn btn-warning'
+                    onClick={()=>{onToggle(index)}}
+                    >Toggle</button>
+                </li>
+            ))}
+        </ul>
+    </div>
   )
 }
 
