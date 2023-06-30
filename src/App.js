aeac250161e64bde7cb21339a4cffde2cb38569c
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-06-30 10:13:31 +0530

    create list and toggle function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+
+import { useState } from 'react';
 import './App.css';
+import ToDoForm from './components/ToDoForm/ToDoForm';
+import ToDoList from './components/ToDoList/ToDoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const createTodo = (text)=>{
+    setTodos([...todos, { id: todos.length + 1, text, completed: false}]);
+  }
+
+  const toggleTodo = (index)=>{
+    const list = [...todos];
+    list[index].completed = !list[index].completed;
+    setTodos(list);
+  }
   return (
     <div className="App">
+      <h1>To Do App</h1>
+      <ToDoForm onCreateTodo={createTodo} />
+      <ToDoList todos={todos} onToggle={toggleTodo} />
     </div>
   );
 }
