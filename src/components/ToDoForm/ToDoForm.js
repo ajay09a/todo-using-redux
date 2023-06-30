50f3619bfd2bb7917fb865b105a3df982200e6e6
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-06-30 10:31:05 +0530

    complete todoform component

--- src/components/ToDoForm/ToDoForm.js
+++ src/components/ToDoForm/ToDoForm.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState } from "react";
+import "./ToDoForm.css";
 
-function ToDoForm() {
+function ToDoForm({ onCreateTodo }) {
+    const [todoText, setTodoText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onCreateTodo(todoText);
+    setTodoText("");
+  };
+
   return (
-    <div>ToDoForm</div>
+    <div className="container">
+      
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="form-control mb-3"
+        value={todoText}
+        onChange={(e) => setTodoText(e.target.value)}
+      />
+      <button className="btn btn-success float-end" type="submit">Create Todo</button>
+    </form>
+    </div>
   )
 }
 
